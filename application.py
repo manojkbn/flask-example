1083346d90135179d310a1f5e4ebc907930477da
Manoj Kozhikkal <manojkbn@gmail.com>
2022-12-23 00:18:37 +0530

    redirect urls

--- application.py
+++ application.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 application = Flask(__name__)
 
 @application.route('/')
-def hello_world():
-    return 'Hello Manoj'
+def hello_admin():
+    return 'Hello Admin'
+
+@application.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello %s as Guest' % guest
+
+@application.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest = name))
+
